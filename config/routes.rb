83c4912202793858f3fc373ff20c969622589d69
buddhamagnet <buddhamagnet@gmail.com>
2014-09-30 20:20:51 +0100

    build in foundation framework and front end tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,6 @@
 Rails.application.routes.draw do
-  get 'feed_item/index'
 
-  get 'feed_item/show'
-
-  get 'user/index'
-
-  get 'user/new'
-
-  get 'user/create'
-
-  get 'user/show'
-
-  get 'user/edit'
-
-  get 'user/update'
-
-  get 'user/destroy'
+  root 'users#index'
 
   resources :users do
     resources :feed_items
