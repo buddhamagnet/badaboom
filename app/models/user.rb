afa43dbfa1db570b556037ae2fef01425a9b5b79
buddhamagnet <buddhamagnet@gmail.com>
2014-09-30 08:17:56 -0400

    add validations and feed url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+
+  def feed
+    "https://audioboom.com/users/#{uid}/boos.rss"
+  end
+
+  validates :uid, presence: true
+  validates :username, presence: true
 end
