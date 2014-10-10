c0809d840035223efa1990b9b2332667a54a5ac9
buddhamagnet <buddhamagnet@gmail.com>
2014-10-10 23:04:25 +0100

    amend views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
-User.create!(uid: 124, username: 'documentally')
-User.create!(uid: 229, username: 'buddhamagnet')
+User.find_or_create_by(uid: 124, username: 'documentally')
+User.find_or_create_by(uid: 121, username: 'sizemore')
+User.find_or_create_by(uid: 202, username: 'philcampbell')
+User.find_or_create_by(uid: 229, username: 'buddhamagnet')
+User.find_or_create_by(uid: 50478, username: 'GeoffTidey')
+
+
